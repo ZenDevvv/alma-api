654b15300d21432925af8a53101d05e3be81a280
ZenDevvv <ozenandreiiii@gmail.com>
2026-02-10 21:53:16 +0800

    remove unused seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,17 +1,7 @@
 import { PrismaClient } from "../generated/prisma";
-import { GenerateUser } from "./seeds/generate-user";
-import { GenerateProducts } from "./seeds/generate-products";
-import { GenerateSuppliers } from "./seeds/generate-suppliers";
-import { seedOrganization } from "./seeds/organizationSeeder";
 const prisma = new PrismaClient();
 
 async function main() {
-	// await GenerateSuperadmin();
-	// await GenerateProducts();
-	await GenerateProducts();
-	await seedOrganization();
-	await GenerateUser(); //this seed generate user, department, and stock records
-	await GenerateSuppliers();
 
 	console.log("Seeding completed successfully!");
 }
