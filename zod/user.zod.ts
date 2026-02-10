f71606ff4176b632994a82d4d2422c5643c90061
ZenDevvv <ozenandreiiii@gmail.com>
2026-02-10 16:57:30 +0800

    fix

--- zod/user.zod.ts
+++ zod/user.zod.ts
@@ -4,22 +4,13 @@ import { isValidObjectId } from "mongoose";
 // Match your Prisma enum values exactly
 export const Role = z.enum(["user", "admin", "viewer"]);
 
-export const SubRole = z.enum([
-	"learner",
-	"instructor",
-	"org_admin",
-]);
+export const SubRole = z.enum(["learner", "instructor", "org_admin"]);
 
 export const Status = z.enum(["active", "inactive", "suspended", "archived"]);
 
 // User Schema (full, including ID)
 export const UserSchema = z.object({
 	id: z.string().refine((val) => isValidObjectId(val)),
-	personId: z
-		.string()
-		.refine((val) => isValidObjectId(val))
-		.nullable()
-		.optional(),
 	avatar: z.string().optional(),
 	userName: z.string().optional(),
 	email: z.string().min(1),
@@ -32,6 +23,11 @@ export const UserSchema = z.object({
 	loginMethod: z.string().min(1),
 	createdAt: z.coerce.date(),
 	updatedAt: z.coerce.date(),
+	personId: z
+		.string()
+		.refine((val) => isValidObjectId(val))
+		.nullable()
+		.optional(),
 	orgId: z
 		.string()
 		.refine((val) => isValidObjectId(val))
